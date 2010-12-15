f8471dcab0bbc186cb95a0662e65d2d392e9b729
Jeff Ching <ching.jeff@gmail.com>
2010-12-15 15:32:38 -0800

    gemifying

--- test/unit/chart_test.rb
+++ test/unit/chart_test.rb
@@ -7,7 +7,7 @@ class ChartTest < ZillowTest
     mock_http_response("chart.xml")
 
     response = Zillow4r.fetch_chart("unit-type" => "percent", "zpid" => 48749425, "width" => 300, "height" => 150)
-    assert_equal("http://www.zillow.com/app?chartDuration=1year&chartType=partner&height=150&page=webservice%2FGetChart&service=chart&showPercent=true&width=300&zpid=48749425", response.url)
+    assert_equal("http://www.zillow.com/app?chartDuration=1year&chartType=partner&height=150&page=webservice%2FGetChart&service=chart&showPercent=true&width=300&zpid=48749425", response.chart_url)
   end
 
 end
